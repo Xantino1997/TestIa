8404fde5cb6956ba68d42ebe0019fb274f2ee59b
Cirene1997 <gjoaquinreynoso@gmail.com>
2024-09-02 18:18:39 -0300

    add the origin: ['https://test-ia-gamma.vercel.app']

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const app = require('./app'); // Importar la configuración de la aplicación desde app.js
 const http = require('http');
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: ['https://test-ia-gamma.vercel.app']
+})
+);
 
 
